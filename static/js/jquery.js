3f5cbc7c66feff31788c9759517f2a50c6cbaece
is23 <37001229+is23@users.noreply.github.com>
2019-02-09 22:42:33 -0500

    fixed git mistake

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function(){
 
     $("button.main-button").click(function(){
@@ -38,44 +37,3 @@ $(document).ready(function(){
     });
 
 });
-=======
-$(document).ready(function(){
-
-    $("button.main-button").click(function(){
-        console.log($(this).text());
-        if($(this).text() == "Projects") {
-            $(".skills-section").hide();
-            $(".work-section").hide();
-            $(".project-section").show();
-        }
-        else if($(this).text() == "Work Experience") {
-            $(".skills-section").hide();
-            $(".project-section").hide();
-            $(".work-section").show();
-        }
-        else if($(this).text() == "Skills") {
-            $(".work-section").hide();
-            $(".project-section").hide();
-            $(".skills-section").show();
-        }
-
-
-    });
-
-    $("button.collapsible").click(function(){
-        if($(this).text() == "Student Service Website") {
-            $(".project1").toggle();
-        }
-        else if($(this).text() == "Online Banking Website") {
-            $(".project2").toggle();
-        }
-
-    });
-
-    $("").click(function() {
-        $(this).hide();
-        alert($(this).text());
-    });
-
-});
->>>>>>> f6177ed5c1d334fb48e53a8f3dc104d8ec347077
